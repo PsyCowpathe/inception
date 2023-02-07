ba65ea07381701c7eecdd83e2580474e825ceb7d
Agirona <agirona@student.42lyon.fr>
2023-02-07 16:55:50 +0100

    Finished all comment, I will now look for useless operation to remove

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+#Start mysqld in safe mode meaning that it will restart in case of error
+
 mysqld_safe &
+
+#Waiting for previous command to finish execution
 
 sleep 3
 
+#Start mysql command line as root then create the database and enable unicode.
+#Collate is used to sort and provide some specific properties to the database
+
 /usr/bin/mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+
+#Provide all privilege to user on all db and create this user, yeah ... in that order its kinda weird
+
 /usr/bin/mysql -uroot -e "GRANT ALL ON $DB_NAME .* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PSWD';"
+
+#Setup the password for root user
+
 /usr/bin/mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PSWD';"
 
-mysqladmin -uroot -p$DB_ROOT_PSWD shutdown
+#Using root user to shutdown the database
+
+mysqladmin -uroot -p $DB_ROOT_PSWD shutdown
+
+#Run the database 
 
 /usr/sbin/mysqld
